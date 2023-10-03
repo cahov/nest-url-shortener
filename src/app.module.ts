6fe3691e49ba5b2638e92697345b814992f33838
cahov <christian.herrera.onaindi@gmail.com>
2023-10-03 16:24:37 -0400

    feat: add Prisma orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ShortenerModule } from './shortener/shortener.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ShortenerModule, PrismaModule],
   controllers: [],
   providers: [],
 })
